3fea80db9df8e7779767d5586e1644b6410120cb
suraj <surajlokhande7744@gmail.com>
2023-12-13 18:20:07 +0530

    phonepe

---
+++ crosel using a interval/crosel.js
@@ -0,0 +1,38 @@
+// slider = document.querySelector(".slider");
+// let img = document.querySelector("img");
+// let btnprev = document.querySelector(".btnprev");
+// let btnnext = document.querySelector(".btnnext");
+
+let arr = [
+  "https://tse1.mm.bing.net/th?id=OIP.piNxVkqzJVpohHF5bwpWzgHaEJ&pid=Api&P=0&h=180",
+  "https://tse3.mm.bing.net/th?id=OIP.WXOyi2RkWruFvwKiO-y6ewHaEK&pid=Api&P=0&h=180",
+  "https://tse2.mm.bing.net/th?id=OIP.akz0hTxRH7ofWzOzjjH3zQHaDn&pid=Api&P=0&h=180",
+];
+
+let ind = 0;
+
+// arr.forEach(function (ele) {
+//   let ans = setInterval(function () {
+//     if(ind == 3){
+//         ind = 0;
+//     }
+//     btnprev.addEventListener("click", function(){
+//         ind -= 1;
+//         console.log(ind);
+//     })
+//     let image = document.createElement("img");
+//     image.setAttribute("src", arr[ind++]);
+
+//     slider.append(image);
+    
+//     setTimeout(function () {
+//         image.removeAttribute("src");        
+//     }, 950)
+
+// }, 1000);
+// });
+// setTimeout(function () {
+//     clearInterval(ans)
+//     console.log("clear")
+// },10000)
+
